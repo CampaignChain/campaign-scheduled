ed1d0bfdbf8f194708f69cd90bfd51e4c366044d
Sandro Groganz <sandro@campaignchain.com>
2014-11-20 18:02:25 +0100

    CE-120 Rename "Timeline" and "Calendar" headings to "Plan"

--- Controller/PlanController.php
+++ Controller/PlanController.php
@@ -18,7 +18,7 @@ class PlanController extends Controller
         return $this->render(
             'CampaignChainCoreBundle:Plan/Timeline:index.html.twig',
             array(
-                'page_title' => 'Timeline',
+                'page_title' => 'Plan',
                 'gantt_tasks' => $this->get('campaignchain.core.model.dhtmlxgantt')->getTasks(),
                 'gantt_toolbar_status' => 'default',
                 'path_embedded' => $this->generateUrl('campaignchain_campaign_scheduled_plan_timeline'),
@@ -30,7 +30,7 @@ class PlanController extends Controller
         return $this->render(
             'CampaignChainCoreBundle:Plan/Timeline:fullscreen.html.twig',
             array(
-                'page_title' => 'Timeline',
+                'page_title' => 'Plan in Timeline',
                 'gantt_tasks' => $this->get('campaignchain.core.model.dhtmlxgantt')->getTasks(),
                 'gantt_toolbar_status' => 'fullscreen',
                 'path_embedded' => $this->generateUrl('campaignchain_campaign_scheduled_plan_timeline'),
@@ -42,7 +42,7 @@ class PlanController extends Controller
         return $this->render(
             'CampaignChainCoreBundle:Plan/Calendar:index.html.twig',
             array(
-                'page_title' => 'Calendar',
+                'page_title' => 'Plan',
                 'events' => $this->get('campaignchain.core.model.fullcalendar')->getEvents(),
             ));
     }
