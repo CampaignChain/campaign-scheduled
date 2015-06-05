c78be23720db98a6f8906ce5b8c928fd47ced906
Sandro Groganz <sandro@campaignchain.com>
2015-06-05 20:01:38 +0200

    CE-299 Campaign template

--- Controller/PlanController.php
+++ Controller/PlanController.php
@@ -11,6 +11,7 @@
 namespace CampaignChain\Campaign\ScheduledCampaignBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class PlanController extends Controller
 {
@@ -53,6 +54,31 @@ class PlanController extends Controller
                 'events' => $this->get('campaignchain.core.model.fullcalendar')->getEvents(
                         self::BUNDLE_NAME, self::MODULE_IDENTIFIER
                     ),
+            ));
+    }
+
+    public function timelineDetailAction(Request $request, $id){
+        /*
+         * Set current campaign in session, e.g. to pre-fill the campaign field
+         * in a new activity with it.
+         */
+        $this->get('session')->set('campaignchain.campaign', $id);
+
+        $campaignService = $this->get('campaignchain.core.campaign');
+        $campaign = $campaignService->getCampaign($id);
+
+        return $this->render(
+            'CampaignChainCampaignScheduledCampaignBundle:Plan/Timeline:detail.html.twig',
+            array(
+                'page_title' => 'Plan Scheduled Campaign',
+                'gantt_tasks' => $this->get('campaignchain.core.model.dhtmlxgantt')->getTasks(
+                        self::BUNDLE_NAME, self::MODULE_IDENTIFIER, $id
+                    ),
+                'gantt_toolbar_status' => 'default',
+                'path_embedded' => '',
+                'path_fullscreen' =>  '',
+                'scale_start_date' => $campaign->getStartDate()->format(\DateTime::ISO8601),
+                'scale_end_date' => $campaign->getEndDate()->format(\DateTime::ISO8601),
             ));
     }
 }
